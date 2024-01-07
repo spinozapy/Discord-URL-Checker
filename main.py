1b1128ef5cca4b039470bb62788719e85119da29
Spinoza <130261872+spinozapy@users.noreply.github.com>
2024-01-07 19:28:38 +0300

    main.py

--- main.py
+++ main.py
@@ -31,22 +31,27 @@ while True:
                 data = response.json()
                 os.system(clear_command)
                 print("")
-                print(colorama.Fore.YELLOW + f" Vanity URL Code: " + colorama.Fore.LIGHTWHITE_EX + str(data['guild'].get('vanity_url_code', 'None')))
-                print(colorama.Fore.YELLOW + f" Premium Subscription Count (Boosts): " + colorama.Fore.LIGHTWHITE_EX + str(data['guild'].get('premium_subscription_count', 'None')))
+                print(colorama.Fore.YELLOW + f" Vanity URL   : " + colorama.Fore.LIGHTWHITE_EX + str(data['guild'].get('vanity_url_code', 'None')))
+                print(colorama.Fore.YELLOW + f" Boosts Count : " + colorama.Fore.LIGHTWHITE_EX + str(data['guild'].get('premium_subscription_count', 'None')))
                 print("")
-                print(colorama.Fore.YELLOW + f" Guild ID: " + colorama.Fore.LIGHTWHITE_EX + str(data['guild']['id']))
-                print(colorama.Fore.YELLOW + f" Guild Name: " + colorama.Fore.LIGHTWHITE_EX + str(data['guild']['name']))
-                print(colorama.Fore.YELLOW + f" Guild Description: " + colorama.Fore.LIGHTWHITE_EX + str(data['guild'].get('description', 'None')))
+                print(colorama.Fore.YELLOW + f" Server ID    : " + colorama.Fore.LIGHTWHITE_EX + str(data['guild']['id']))
+                print(colorama.Fore.YELLOW + f" Server Name  : " + colorama.Fore.LIGHTWHITE_EX + str(data['guild']['name']))
+                print(colorama.Fore.YELLOW + f" Description  : " + colorama.Fore.LIGHTWHITE_EX + str(data['guild'].get('description', 'None')))
                 print("")
                 banner_extension = "gif" if data['guild'].get('banner', '').startswith("a_") else "png"
                 banner_url = f"https://cdn.discordapp.com/banners/{data['guild']['id']}/{data['guild'].get('banner', '')}.{banner_extension}" if data['guild'].get('banner') else "None"
-                print(colorama.Fore.YELLOW + f" Banner URL: " + colorama.Fore.LIGHTWHITE_EX + str(banner_url))
+                print(colorama.Fore.YELLOW + f" Banner URL   : " + colorama.Fore.LIGHTWHITE_EX + str(banner_url))
                 icon_extension = "gif" if data['guild'].get('icon', '').startswith("a_") else "png"
                 icon_url = f"https://cdn.discordapp.com/icons/{data['guild']['id']}/{data['guild'].get('icon', '')}.{icon_extension}" if data['guild'].get('icon') else "None"
-                print(colorama.Fore.YELLOW + f" Icon URL: " + colorama.Fore.LIGHTWHITE_EX + str(icon_url))
+                print(colorama.Fore.YELLOW + f" Icon URL     : " + colorama.Fore.LIGHTWHITE_EX + str(icon_url))
                 splash_extension = "png"
                 splash_url = f"https://cdn.discordapp.com/splashes/{data['guild']['id']}/{data['guild'].get('splash', '')}.{splash_extension}" if data['guild'].get('splash') else "None"
-                print(colorama.Fore.YELLOW + f" Splash URL: " + colorama.Fore.LIGHTWHITE_EX + str(splash_url))
+                print(colorama.Fore.YELLOW + f" Splash URL   : " + colorama.Fore.LIGHTWHITE_EX + str(splash_url))
+
+                print("")
+                print(colorama.Fore.YELLOW + f" Channel Name : " + colorama.Fore.LIGHTWHITE_EX + f"#{data['channel']['name']}")
+                print(colorama.Fore.YELLOW + f" Channel ID   : " + colorama.Fore.LIGHTWHITE_EX + f"{data['channel']['id']}")
+                print(colorama.Fore.YELLOW + f" Invite Link  : " + colorama.Fore.LIGHTWHITE_EX + f"https://discord.gg/{data['code']}")
             else:
                 os.system(clear_command)
                 print(colorama.Fore.GREEN + "[URL Checker]: " + colorama.Fore.RED + "Invite link is invalid!")
